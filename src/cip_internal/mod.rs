9d3e764145c2d6e46368099e8b4d116867f2e595
Malu Gubolin <mlgubolin@protonmail.com>
2019-01-29 12:02:11 +0100

    defined public structs

---
+++ src/cip_internal/mod.rs
@@ -0,0 +1,1 @@
+mod cip_internal;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+mod cip_internal;
+use std::vec::Vec;
+
+pub struct grid_setup {
+    current_grid: grid_variables,
+    previous_grid: grid_variables,
+    next_grid: grid_variables,
+}
+
+struct grid_variables {
+    f: Vec<f64>,
+    u: Vec<f64>,
+    g: Vec<f64>,
+    dx: f64,
+    dt: f64,
+}
+
+// Returns f^{n+1}_i
+pub fn cip_1d(grid_setup: grid_setup) -> Vec<f64> {
+    return vec![0.1, 1.1];
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

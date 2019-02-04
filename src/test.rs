8e4682b685f12696299b2ccfda3f2aa599caceae
Malu Gubolin <mlgubolin@protonmail.com>
2019-02-04 10:09:55 +0100

    uploading to cargo

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,15 @@
 #[cfg(test)]
+mod cip_internal;
 mod tests {
+    let current = GridParameters::new(
+        f: vec![0.1],
+        u: vec![0.1],
+        g: vec![0.1],
+        dx: 1.0,
+        dt: 1.0
+    )
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_works(current: current) {
+        assert_eq!(current.f, vec![0.1]);
     }
 }
